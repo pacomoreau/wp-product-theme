cc92ddfc5cc23344853e8cd9aca9d06ae3b53848
paco <pacomoreau@hotmail.com>
2016-11-04 15:04:39 +0100

    fix #5 Move jQuery in footer

--- web/wp-content/themes/wp-product-theme/helpers/helpers.php
+++ web/wp-content/themes/wp-product-theme/helpers/helpers.php
@@ -34,6 +34,21 @@ function wppt_login_message() {
 add_filter('login_errors', 'wppt_login_message');
 
 /**
+ * Move jQuery in footer.
+ * @param $wp_scripts
+ */
+function wppt_move_jquery_into_footer($wp_scripts) {
+  if (is_admin()) {
+    return;
+  }
+
+  $wp_scripts->add_data('jquery', 'group', 1);
+  $wp_scripts->add_data('jquery-core', 'group', 1);
+  $wp_scripts->add_data('jquery-migrate', 'group', 1);
+}
+add_action('wp_default_scripts', 'wppt_move_jquery_into_footer');
+
+/**
  * Remove accents in media file name.
  * @param $filename
  * @return string
